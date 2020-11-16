fa9d85b57f6415baeb45d35467a5f388397b6546
zerodeusx <zed14lvl@gmail.com>
2020-11-17 01:14:36 +0200

    Обчислення У

---
+++ lab04/task05/src/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int X = 0;	//Correct this for result					
+    int y;
+
+    if (X== 3) {                    //Just giving Y the value with the help of the graph 
+        y=2;
+    }
+
+    else if (X == 2) {             
+        y=1;
+
+    }
+
+    else if (X == 1) {              
+        y=0;
+    }
+    else if (X == 0) {               
+        y=1;
+    }
+    else if (X == -1) {          
+        y=0;
+    }
+    else if (X == -2) {              
+        y=1;
+    }
+    else if (X == -3) {         
+        y=2;
+    }
+    else{                           //Test
+    }
+
+    return 0;
+}
